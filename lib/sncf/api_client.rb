9567b276bf23b7a409102af5e05ea441f233d4f5
Rémi Delhaye <remi.delhaye@epsi.fr>
2015-06-29 10:54:18 +0200

    Fix bug on class end

--- lib/sncf/api_client.rb
+++ lib/sncf/api_client.rb
@@ -3,17 +3,15 @@ require 'oj'
 
 module Sncf
   class ApiClient
-    HOST_URL          = 'https://api.sncf.com'
-    BASE_PATH         = 'v1'
-    PLAN = {
+    BASE_URL      = 'https://api.sncf.com'
+    VERSION_PATH  = 'v1'
+    PLAN          = {
       name: 'Free',
       limits: {
         per_day: 3_000,
         per_month: 90_000
       }
     }
-
-    # => 3aa53f6d-13f1-486b-8a68-a781842cbe73
 
     attr_reader :api_key
 
@@ -27,7 +25,10 @@ module Sncf
 
     def quotas
       @quotas ||= begin
-        { per_day: PLAN[:limits][:per_day], per_month: PLAN[:limits][:per_month] }
+        {
+          per_day: PLAN[:limits][:per_day],
+          per_month: PLAN[:limits][:per_month]
+        }
       end
     end
 
@@ -45,9 +46,10 @@ module Sncf
 
     def construct_formated_url(path, additional_params)
       if additional_params.nil?
-        "#{HOST_URL}/#{BASE_PATH}/#{path}"
+        "#{BASE_URL}/#{VERSION_PATH}/#{path}"
       else
-        "#{HOST_URL}/#{BASE_PATH}/#{path}?#{additional_params.to_query}"
+        "#{BASE_URL}/#{VERSION_PATH}/#{path}?#{additional_params.to_query}"
       end
+    end
   end
 end
